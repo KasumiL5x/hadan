724092546491cb0c2d6e76e0cf205adc38c07f34
Daniel Green <KasumiL5x@gmail.com>
2016-01-12 21:24:52 +0000

    Testing adding an MFnMesh to the Maya scene.  Seems to work fine (albeit without a material applied).  It's surprisingly clean for the Maya API!

--- src/hadan.cpp
+++ src/hadan.cpp
@@ -1,9 +1,32 @@
 #include <maya/MIOStream.h>
 #include <maya/MSimple.h>
+#include <maya/MFnMesh.h>
+#include <maya/MPointArray.h>
 
 DeclareSimpleCommand(hadan, "KasumiL5x", "0.0.1-dev");
 
 MStatus hadan::doIt( const MArgList& args ) {
-	std::cout << "Hello, " << args.asString(0).asChar() << std::endl;
+	std::cout << "Hello there, " << args.asString(0).asChar() << std::endl;
+
+	MPointArray pointArray;
+	pointArray.append(MPoint(-0.5f, -0.5f, 0.0f));
+	pointArray.append(MPoint( 0.5f, -0.5f, 0.0f));
+	pointArray.append(MPoint(-0.5f,  0.5f, 0.0f));
+	pointArray.append(MPoint( 0.5f,  0.5f, 0.0f));
+	const int numVerts = pointArray.length();
+
+	MIntArray faceCounts;
+	faceCounts.append(3);
+	faceCounts.append(3);
+
+	MIntArray faceConnects;
+	faceConnects.append(0); faceConnects.append(1); faceConnects.append(2);
+	faceConnects.append(1); faceConnects.append(3); faceConnects.append(2);
+
+	const int numFaces = faceConnects.length() / 3;
+
+	MFnMesh mesh;
+	MObject dataObj = mesh.create(numVerts, numFaces, pointArray, faceCounts, faceConnects);
+
 	return MStatus::kSuccess;
 }
