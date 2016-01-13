23a9131af4000aa29de3bea7fcc107c1ef1dc188
Daniel Green <KasumiL5x@gmail.com>
2016-01-13 07:47:45 +0000

    Added ccmath submodule and made the created mesh now be a cube!  Don't forget to init and update the submodules when pulling a fresh repo!

--- src/hadan.cpp
+++ src/hadan.cpp
@@ -9,19 +9,38 @@ MStatus hadan::doIt( const MArgList& args ) {
 	std::cout << "Hello there, " << args.asString(0).asChar() << std::endl;
 
 	MPointArray pointArray;
-	pointArray.append(MPoint(-0.5f, -0.5f, 0.0f));
-	pointArray.append(MPoint( 0.5f, -0.5f, 0.0f));
-	pointArray.append(MPoint(-0.5f,  0.5f, 0.0f));
-	pointArray.append(MPoint( 0.5f,  0.5f, 0.0f));
+	const float SIZE = 1.0f;
+	pointArray.append(MPoint(-0.5f*SIZE, -0.5f*SIZE,  0.5f*SIZE)); // front
+	pointArray.append(MPoint( 0.5f*SIZE, -0.5f*SIZE,  0.5f*SIZE));
+	pointArray.append(MPoint( 0.5f*SIZE,  0.5f*SIZE,  0.5f*SIZE));
+	pointArray.append(MPoint(-0.5f*SIZE,  0.5f*SIZE,  0.5f*SIZE));
+	pointArray.append(MPoint(-0.5f*SIZE, -0.5f*SIZE, -0.5f*SIZE)); // back
+	pointArray.append(MPoint( 0.5f*SIZE, -0.5f*SIZE, -0.5f*SIZE));
+	pointArray.append(MPoint( 0.5f*SIZE,  0.5f*SIZE, -0.5f*SIZE));
+	pointArray.append(MPoint(-0.5f*SIZE,  0.5f*SIZE, -0.5f*SIZE));
 	const int numVerts = pointArray.length();
 
 	MIntArray faceCounts;
-	faceCounts.append(3);
-	faceCounts.append(3);
+	faceCounts.append(3); faceCounts.append(3);
+	faceCounts.append(3); faceCounts.append(3);
+	faceCounts.append(3); faceCounts.append(3);
+	faceCounts.append(3); faceCounts.append(3);
+	faceCounts.append(3); faceCounts.append(3);
+	faceCounts.append(3); faceCounts.append(3);
 
 	MIntArray faceConnects;
-	faceConnects.append(0); faceConnects.append(1); faceConnects.append(2);
-	faceConnects.append(1); faceConnects.append(3); faceConnects.append(2);
+	faceConnects.append(0); faceConnects.append(1); faceConnects.append(2); // front
+	faceConnects.append(2); faceConnects.append(3); faceConnects.append(0);
+	faceConnects.append(1); faceConnects.append(5); faceConnects.append(6); // top
+	faceConnects.append(6); faceConnects.append(2); faceConnects.append(1);
+	faceConnects.append(7); faceConnects.append(6); faceConnects.append(5); // back
+	faceConnects.append(5); faceConnects.append(4); faceConnects.append(7);
+	faceConnects.append(4); faceConnects.append(0); faceConnects.append(3); // bottom
+	faceConnects.append(3); faceConnects.append(7); faceConnects.append(4);
+	faceConnects.append(4); faceConnects.append(5); faceConnects.append(1); // left
+	faceConnects.append(1); faceConnects.append(0); faceConnects.append(4);
+	faceConnects.append(3); faceConnects.append(2); faceConnects.append(6); // right
+	faceConnects.append(6); faceConnects.append(7); faceConnects.append(3);
 
 	const int numFaces = faceConnects.length() / 3;
 
