f181c44723f43fdb3d1d4f36f1040d1f1ae8145c
Daniel Green <KasumiL5x@gmail.com>
2016-01-15 12:25:12 +0000

    Added a log class and started changing prints to use it.  Also now print the start time and stats at the end.

--- src/hadan.cpp
+++ src/hadan.cpp
@@ -18,6 +18,7 @@
 //    - add ability to push vertices along negative normals by a fraction to shrink mesh (once normals are generated properly), mainly for stacking
 //    - preserve UVs and somehow add a separate material on the inside
 
+#include <chrono>
 #include <ctime>
 #include <maya/MSimple.h>
 #include <maya/MFnMesh.h>
@@ -29,6 +30,7 @@
 #include "TestSamplePointGenerator.hpp"
 #include "VoronoiPlaneGenerator.hpp"
 #include "PlaneHelper.hpp"
+#include "Log.hpp"
 
 DeclareSimpleCommand(hadan, "KasumiL5x", "0.0.1-dev");
 
@@ -55,41 +57,55 @@ void destroy() {
 }
 
 MStatus hadan::doIt( const MArgList& args ) {
-	srand(static_cast<unsigned int>(time(0)));
+	// get start time
+	const auto startTime = std::chrono::system_clock::now();
 
-	printf("Hadan beginning!\n");
+	// print start time
+	const std::time_t epochTime = std::chrono::system_clock::to_time_t(startTime);
+	//const std::string startTimeStr = std::ctime(&epochTime);
+	char startTimeStr[100];
+	std::strftime(startTimeStr, sizeof(startTimeStr), "%X", std::localtime(&epochTime));
+	Log::info("Hadan starting at " + std::string(startTimeStr) + "\n");
 
+	// randomize seed
+	srand(static_cast<unsigned int>(startTime.time_since_epoch().count()));
+
+	// check arguments length
 	if( args.length() < 2 ) {
-		displayError("Hadan ERROR: Incorrect number of arguments.");
-		return MS::kFailure; // must have right number of arguments
+		Log::error("Error: Incorrect number of arguments.\n");
+		return MS::kFailure;
 	}
 
 	// get and validate number of slices
 	const int numSlices = args.asInt(1);
 	if( numSlices < 1 ) {
-		displayError("Hadan ERROR: Must have at least one slice!");
+		Log::error("Error: Must have at least one slice!\n");
 		return MS::kFailure;
 	}
 
+	// get the input object's dag path and check that it exists
 	MDagPath inputObjectPath;
 	if( !MayaHelper::getObjectFromString(args.asString(0).asChar(), inputObjectPath) ) {
-		displayError("Hadan ERROR: Object not found.");
-		return MS::kFailure; // object must exist
-	}
-
-	if( !MayaHelper::hasMesh(inputObjectPath) ) {
-		displayError("Hadan ERROR: Object is not a mesh.");
-		return MS::kFailure; // object must be a mesh
-	}
-
-	MFnMesh mayaMesh(inputObjectPath);
-	if( MayaHelper::doesMeshHaveHoles(mayaMesh) ) {
-		displayError("Hadan ERROR: Mesh must be a closed convex polyhedron (no holes; welded closed)."); // object must be closed
+		Log::error("Error: Given object not found.\n");
 		return MS::kFailure;
 	}
 
+	// check that the input object has a mesh fn
+	if( !MayaHelper::hasMesh(inputObjectPath) ) {
+		Log::error("Error: Given object is not a mesh.\n");
+		return MS::kFailure;
+	}
+
+	// convert the input object to a mesh and check if it has holes
+	MFnMesh mayaMesh(inputObjectPath);
+	if( MayaHelper::doesMeshHaveHoles(mayaMesh) ) {
+		Log::error("Error: Mesh cannot have holes.\n");
+		return MS::kFailure;
+	}
+
+	// ensure that the mesh is fully closed (all edges must have two faces)
 	if( !MayaHelper::isMeshFullyClosed(inputObjectPath) ) {
-		displayError("Hadan ERROR: Mesh is not closed.  An edge somewhere only has a single face.");
+		Log::error("Error: Mesh is not closed.  An edge somewhere has only a single face.\n");
 		return MS::kFailure;
 	}
 
@@ -98,17 +114,13 @@ MStatus hadan::doIt( const MArgList& args ) {
 	MayaHelper::copyMFnMeshToModel(mayaMesh, fromMaya);
 	fromMaya.buildExtendedData();
 
-	// debug: print bb info
-	BoundingBox bb = fromMaya.computeBoundingBox();
-	printf("minx(%f), maxx(%f), miny(%f), maxy(%f), minz(%f), maxz(%f)\n", bb.minX(), bb.maxX(), bb.minY(), bb.maxY(), bb.minZ(), bb.maxZ());
-
 	// create a sample point generator and generate sample points
 	pointGenerator = createSamplePointGenerator();
 	std::vector<cc::Vec3f> samplePoints;
 	pointGenerator->generateSamplePoints(fromMaya, static_cast<unsigned int>(numSlices), samplePoints);
 
 	if( samplePoints.empty() ) {
-		printf("Hadan ERROR: Not enough sample points were generated.\n");
+		Log::error("Error: Not enough sample points were generated.\n");
 		destroy();
 		return MS::kFailure;
 	}
@@ -121,7 +133,7 @@ MStatus hadan::doIt( const MArgList& args ) {
 	planeGenerator->generatePlanes(fromMaya.computeBoundingBox(), samplePoints, outPlanes, outPlaneCounts, outCellPositions);
 
 	if( outPlanes.empty() || outPlaneCounts.empty() ) {
-		printf("Hadan ERROR: Generated cutting planes were inadequate.\n");
+		Log::error("Error: Generated cutting planes were inadequate.\n");
 		destroy();
 		return MS::kFailure;
 	}
@@ -160,12 +172,10 @@ MStatus hadan::doIt( const MArgList& args ) {
 				MayaHelper::copyModelToMFnMesh(clippedModel, outCellChunkMesh);
 				allGeneratedMeshes.push_back(outCellChunkMesh.object());
 			} else {
-				printf("Hadan ERROR: Failed to convert clipped model.\n");
+				Log::info("Failed to convert clipped mesh, most likely due to clipping leaving insignificant geometry.  This is expected.\n");
 			}
 		}
 	}
-
-	printf("Hadan Complete (%zd/%zd chunks)\n", allGeneratedMeshes.size(), outPlaneCounts.size());
 
 	// modify normals of all meshes (harden then set angle as sometimes only set angle doesn't work)
 	for( const auto& mesh : allGeneratedMeshes ) {
@@ -193,6 +203,14 @@ MStatus hadan::doIt( const MArgList& args ) {
 	MSelectionList sourceObjectSelectionList;
 	sourceObjectSelectionList.add(inputObjectPath);
 	MGlobal::setActiveSelectionList(sourceObjectSelectionList);
+
+
+	// print completion stats
+	const auto endTime = std::chrono::system_clock::now();
+	const std::chrono::duration<double> timeDiff = endTime - startTime;
+	const std::string timeTakenStr = "Hadan finished in " + std::to_string(timeDiff.count()) + "s. ";
+	const std::string chunkStr = std::to_string(allGeneratedMeshes.size()) + "/" + std::to_string(outPlaneCounts.size()) + " chunks generated.\n";
+	Log::info(timeTakenStr + chunkStr);
 
 	destroy();
 
