eacf847054a7f6baf5b55a2e9097b57d9fff0f7c
Daniel Green <KasumiL5x@gmail.com>
2016-01-20 21:44:29 +0000

    Changed UniformPointGen to use PointUtils.

--- src/points/Uniform/UniformPointGen.cpp
+++ src/points/Uniform/UniformPointGen.cpp
@@ -1,5 +1,5 @@
 #include "UniformPointGen.hpp"
-#include <random>
+#include "../PointsUItils.hpp"
 
 UniformPointGen::UniformPointGen()
 	: IPointGen() {
@@ -9,13 +9,8 @@ UniformPointGen::~UniformPointGen() {
 }
 
 void UniformPointGen::generateSamplePoints( const Model& sourceModel, const PointGenInfo& info, std::vector<cc::Vec3f>& outPoints ) {
-	const BoundingBox bb = sourceModel.computeBoundingBox();
-	std::random_device rnd;
-	std::mt19937 gen(rnd());
-	std::uniform_real_distribution<float> dx(bb.minX(), bb.maxX());
-	std::uniform_real_distribution<float> dy(bb.minY(), bb.maxY());
-	std::uniform_real_distribution<float> dz(bb.minZ(), bb.maxZ());
+	const BoundingBox bbox = sourceModel.computeBoundingBox();
 	for( unsigned int i = 0; i < info.uniformCount; ++i ) {
-		outPoints.push_back(cc::Vec3f(dx(gen), dy(gen), dz(gen)));
+		outPoints.push_back(PointsUtils::randomPointInBbox(bbox));
 	}
 }
